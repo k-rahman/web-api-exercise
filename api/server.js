1be7d89f1ffaea39d7bf5e30476067fc9f46b0cb
Karim Abdelrahman <ka_mo_hassan@hotmail.com>
2021-01-29 04:29:41 +0200

    Plug the routes to the server

--- api/server.js
+++ api/server.js
@@ -1,18 +1,29 @@
 const os = require('os');
 const express = require('express');
 const cors = require('cors');
+const db = require('./db/index');
 
 // routes imports
 const items = require('./routes/items');
+const users = require('./routes/users');
+const register = require('./routes/register');
+const auth = require('./routes/auth');
 
 const app = express();
-const PORT = process.env.PORT || 3200;
 
+// middlewares
+app.use(express.json());
 app.use(cors());
 
 // routes
 app.use('/items', items);
+app.use('/users', users);
+app.use('/register', register);
+app.use('/auth', auth);
 
-app.listen(PORT, () => {
+//db.create().catch(e => console.log(e));
+
+const PORT = process.env.PORT || 3200;
+module.exports = app.listen(PORT, () => {
   console.log(`Server is running on http://${os.networkInterfaces().enp5s0[0].address}:${PORT} ...`);
 });

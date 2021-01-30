bf38f53c92d5c1809618e3f10c420df93e6d5869
Karim Abdelrahman <ka_mo_hassan@hotmail.com>
2021-01-31 00:54:20 +0200

    Restructure POST /users response

--- api/routes/register.js
+++ api/routes/register.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const users = require('../services/users');
-const validateUserData = require('../middleware/validate');
+const validate = require('../middleware/validate').validateUserData;
 
 // create new user
-router.post('/', validateUserData, (req, res) => {
+router.post('/', validate, (req, res) => {
   users
     .createUser(req.body)
-    .then(result => res.status(201).send({ code: '201', message: result.insertId.toString() })
-    )
+    .then(result => res.status(201).send({ userId: result.insertId }))
     .catch(e => e.name === 400 ?
       res.status(400).send({ code: '400', message: e.message }) :
       res.sendStatus(500)

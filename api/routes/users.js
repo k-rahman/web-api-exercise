057f9b75de28be1e907ba6b5c73f7cb6d2033317
Karim Abdelrahman <ka_mo_hassan@hotmail.com>
2021-01-31 00:55:37 +0200

    Refactor users routes, to work with the new authenticate function

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,64 +1,39 @@
 const express = require('express');
-const passport = require('passport');
-require('../middleware/auth');
 const router = express.Router();
 const users = require('../services/users');
-const validateUserData = require('../middleware/validate');
+const validate = require('../middleware/validate').validateUserData;
+const authenticate = require('../middleware/auth');
 
 
 // get user information
-router.get('/me', (req, res, next) => {
-  passport.authenticate('jwt', { session: false }, (err, userId) => {
-    if (err) return res.sendStatus(500);
+router.get('/me', authenticate, (req, res) => {
+  const { user } = req;
 
-    userId ?
-      users
-        .getUserById(userId)
-        .then(result => res.status(200).send(result))
-        .catch(e => e.name === 404 ?
-          res.status(404).send({ code: '404', message: e.message }) :
-          res.sendStatus(500)
-        ) :
-      res.status(401).send({ code: '401', message: 'Access token is missing or invalid!' });
-  })(req, res, next);
+  res.status(200).send(user);
 });
 
 // update user
-router.put('/me', validateUserData, (req, res, next) => {
-  passport.authenticate('jwt', (err, userId) => {
-    if (err) return res.sendStatus(500);
+router.put('/me', [validate, authenticate], (req, res) => {
+  const { user } = req;
 
-    userId ?
-      users
-        .updateUser(req.body, userId)
-        .then(_ => res.sendStatus(204))
-        .catch(e => {
-          if (e.name === 404)
-            return res.status(404).send({ code: '404', message: e.message });
-          if (e.name === 400)
-            return res.status(400).send({ code: '400', message: e.message });
-
-          res.sendStatus(500);
-        }) :
-      res.status(401).send({ code: '401', message: 'Access token is missing or invalid!' });
-  })(req, res, next);
+  users
+    .updateUser(req.body, user.userId)
+    .then(_ => res.sendStatus(204))
+    .catch(e => {
+      e.name === 400 ?
+        res.status(400).send({ code: '400', message: e.message }) :
+        res.sendStatus(500);
+    });
 });
 
 // delete user
-router.delete('/me', (req, res, next) => {
-  passport.authenticate('jwt', (err, userId) => {
-    if (err) return res.sendStatus(500);
+router.delete('/me', authenticate, (req, res) => {
+  const { user } = req;
 
-    userId ?
-      users
-        .deleteUser(userId)
-        .then(_ => res.sendStatus(204))
-        .catch(e => e.name === 404 ?
-          res.status(404).send({ code: '404', message: e.message }) :
-          res.sendStatus(500)
-        ) :
-      res.status(401).send({ code: '401', message: 'Access token is missing or invalid!' });
-  })(req, res, next);
+  users
+    .deleteUser(user.userId)
+    .then(_ => res.sendStatus(204))
+    .catch(_ => res.sendStatus(500));
 });
 
 module.exports = router;

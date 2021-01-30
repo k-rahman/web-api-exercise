bb5ebc4a7d04042af2ab0726924298a100c05537
Karim Abdelrahman <ka_mo_hassan@hotmail.com>
2021-01-31 00:52:54 +0200

    Create authenticate function to return 404 if user doesn't exist

--- api/middleware/auth.js
+++ api/middleware/auth.js
@@ -24,12 +24,42 @@ passport.use('httpBasic',
   })
 );
 
+// will throw return false if token is missing
+// check if user exists
 passport.use(new JwtStrategy({
   jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
   secretOrKey: config.get('jwt')
 },
   (payload, done) => {
-    done(null, payload.userId);
+    const { userId } = payload;
+
+    userId ?
+      users
+        .getUserById(payload.userId)
+        .then(user => done(null, user))
+        .catch(e => done(e)) :
+      done(null, false);
   })
 );
 
+// handle if token is invalid/missing
+// handle if user doesn't exist
+const authenticate = (req, res, next) => {
+  passport.authenticate('jwt', (err, user) => {
+    if (err)
+      if (err.name === 404)
+        return res.status(404).send({ code: '404', message: err.message });
+      else
+        return res.sendStatus(500);
+
+    if (user) {
+      req.user = user;
+      next();
+    }
+    else
+      res.status(401).send({ code: '401', message: 'Access token is missing or invalid!' });
+
+  })(req, res, next);
+};
+
+module.exports = authenticate;

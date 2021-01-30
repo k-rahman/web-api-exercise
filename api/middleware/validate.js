839f3f7c54b4abadb65f8bd9e3c9b31dc1ea46a3
Karim Abdelrahman <ka_mo_hassan@hotmail.com>
2021-01-31 00:52:27 +0200

    Add json schema validation to middleware folder

--- api/middleware/validate.js
+++ api/middleware/validate.js
@@ -1,14 +1,38 @@
 const Ajv = require('ajv').default;
-const userSchema = require('../schema/user.json');
+
+const userSchema = require('../schema/newUser.json');
+const itemSchema = require('../schema/newItem.json');
 
 
+const ajv = new Ajv({ strictTuples: false });
+
+// add custom error message to images
+ajv.addKeyword('imageRequired', {
+  validate: validate = (schema, images) => {
+    validate.errors = [{ keyword: 'min', message: 'You must provide at least 1 image' }];
+    return images.length > 0;
+  },
+  errors: true
+});
+
+// check user information sent by client is in the right form
 const validateUserData = (req, res, next) => {
-  const ajv = new Ajv();
-  const validate = ajv.compile(userSchema);
-  const valid = validate(req.body);
+  const valid = ajv.validate(userSchema, req.body);
 
   return valid ?
     next() : res.status(400).send(validate.errors.map(err => err.message));
-}
+};
 
-module.exports = validateUserData;
+// check user information sent by client is in the right form
+const validateNewItem = (req, res, next) => {
+  const valid = ajv.validate(itemSchema, req.body);
+
+  return valid ?
+    next() : res.status(400).send(validate.errors.map(err => err.message));
+};
+
+
+module.exports = {
+  validateUserData,
+  validateNewItem
+}

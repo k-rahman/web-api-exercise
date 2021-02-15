3af135f54f3df2203b011246ce387f1d45b9c8ff
Karim Abdelrahman <ka_mo_hassan@hotmail.com>
2021-02-15 21:37:05 +0200

    Prettier reformatted files

--- api/routes/items.js
+++ api/routes/items.js
@@ -1,69 +1,81 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const items = require('../services/items');
-const { getImages, saveImages } = require('../middleware/images');
-const validate = require('../middleware/validate').validateNewItem;
-const authenticate = require('../middleware/auth');
-const removeImages = require('../utils/images');
-
+const items = require("../services/items");
+const { getImages, saveImages } = require("../middleware/images");
+const validate = require("../middleware/validate").validateNewItem;
+const authenticate = require("../middleware/auth");
+const removeImages = require("../utils/images");
 
 // return all items
-router.get('/', (req, res) => {
-
+router.get("/", (req, res) => {
   items
     .getItems(req.query)
     .then(items => res.status(200).send(items))
-    .catch(e => e.name === 404 ?
-      res.status(404).send({ code: '404', message: e.message }) :
-      res.sendStatus(500));
+    .catch(e =>
+      e.name === 404
+        ? res.status(404).send({ code: "404", message: e.message })
+        : res.sendStatus(500)
+    );
 });
 
 // return item by id
-router.get('/:itemId', (req, res) => {
+router.get("/:itemId", (req, res) => {
   const { itemId } = req.params;
 
   items
     .getItemById(itemId)
     .then(item => res.status(200).send(item))
-    .catch(e => e.name === 404 ?
-      res.status(404).send({ code: '404', message: e.message }) :
-      res.sendStatus(500));
+    .catch(e =>
+      e.name === 404
+        ? res.status(404).send({ code: "404", message: e.message })
+        : res.sendStatus(500)
+    );
 });
 
 // post new item
-router.post('/', [authenticate, getImages, validate, saveImages], (req, res) => {
-  const { user } = req;
+router.post(
+  "/",
+  [authenticate, getImages, validate, saveImages],
+  (req, res) => {
+    const { user } = req;
 
-  items
-    .createNewItem({ ...req.body, sellerId: user.userId })
-    .then(result => res.status(201).send({ itemId: result.insertId }))
-    .catch(_ => {
-      removeImages(req.body.images);
-      res.sendStatus(500);
-    });
-});
+    items
+      .createNewItem({ ...req.body, sellerId: user.userId })
+      .then(result => res.status(201).send({ itemId: result.insertId }))
+      .catch(_ => {
+        removeImages(req.body.images);
+        res.sendStatus(500);
+      });
+  }
+);
 
 // update item
-router.put('/:itemId', [authenticate, getImages, validate, saveImages], (req, res) => {
-  const { itemId } = req.params;
-  const { user } = req;
+router.put(
+  "/:itemId",
+  [authenticate, getImages, validate, saveImages],
+  (req, res) => {
+    const { itemId } = req.params;
+    const { user } = req;
 
-  items
-    .updateItem({ ...req.body, sellerId: user.userId }, itemId)
-    .then(_ => res.sendStatus(204))
-    .catch(e => {
-      removeImages(req.body.images);
+    items
+      .updateItem({ ...req.body, sellerId: user.userId }, itemId)
+      .then(_ => res.sendStatus(204))
+      .catch(e => {
+        removeImages(req.body.images);
 
-      if (e.name === 403) return res.status(403).send({ code: '403', message: e.message });
-      if (e.name === 404) return res.status(404).send({ code: '404', message: e.message });
+        if (e.name === 403)
+          return res.status(403).send({ code: "403", message: e.message });
+        if (e.name === 404)
+          return res.status(404).send({ code: "404", message: e.message });
 
-      res.sendStatus(500);
-    });
-});
+        res.sendStatus(500);
+      });
+  }
+);
 
 // delete item by id
-router.delete('/:itemId', authenticate, (req, res) => {
+router.delete("/:itemId", authenticate, (req, res) => {
   const { itemId } = req.params;
   const { user } = req;
 
@@ -71,8 +83,10 @@ router.delete('/:itemId', authenticate, (req, res) => {
     .deleteItem(itemId, user.userId)
     .then(_ => res.sendStatus(204))
     .catch(e => {
-      if (e.name === 403) return res.status(403).send({ code: '403', message: e.message });
-      if (e.name === 404) return res.status(404).send({ code: '404', message: e.message });
+      if (e.name === 403)
+        return res.status(403).send({ code: "403", message: e.message });
+      if (e.name === 404)
+        return res.status(404).send({ code: "404", message: e.message });
 
       res.sendStatus(500);
     });
--- api/utils/itemsQuery.js
+++ api/utils/itemsQuery.js
@@ -1,7 +1,7 @@
 const getSelectQuery = _ =>
   `SELECT i.itemId, i.title, i.description, i.price, i.country, 
             i.city, concat(u.firstname, ' ', u.lastname) AS 'sellerName',
-            u.phone AS 'contactInfo', i.img1, i.img2, i.img3, i.img4,
+            u.phone AS 'contactInfo', JSON_ARRAY(i.img1, i.img2, i.img3, i.img4) as images,
             c.name AS category, d.name AS deliveryType, i.createdAt, i.updatedAt 
     FROM items i
     JOIN users u ON u.userId = i.seller
@@ -12,11 +12,11 @@ const getSearchQuery = query => {
   let { keyword, category, country, city, date } = query;
   let selectItem = getSelectQuery();
 
-  selectItem += ' WHERE 1 = 1';
+  selectItem += " WHERE 1 = 1";
 
   if (keyword && keyword.length !== 0) {
-    keyword = keyword.trim().replace(' ', '* ');
-    keyword = keyword.padEnd(keyword.length + 1, '*');
+    keyword = keyword.trim().replace(" ", "* ");
+    keyword = keyword.padEnd(keyword.length + 1, "*");
     selectItem += ` AND MATCH (i.title, i.description) AGAINST ('${keyword}' IN BOOLEAN MODE)`;
   }
 
@@ -26,8 +26,7 @@ const getSearchQuery = query => {
   if (country && country.length !== 0)
     selectItem += ` AND i.country LIKE '%${country}%'`;
 
-  if (city && city.length !== 0)
-    selectItem += ` AND i.city LIKE '%${city}%'`;
+  if (city && city.length !== 0) selectItem += ` AND i.city LIKE '%${city}%'`;
 
   if (date && date.length !== 0)
     selectItem += ` AND i.createdAt LIKE '${date}%'`;
@@ -37,5 +36,5 @@ const getSearchQuery = query => {
 
 module.exports = {
   getSelectQuery,
-  getSearchQuery
-}
+  getSearchQuery,
+};

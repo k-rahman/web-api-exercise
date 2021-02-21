6a9333b13415a214e86864229b58d231f2657571
Karim Abdelrahman <ka_mo_hassan@hotmail.com>
2021-02-21 02:56:11 +0200

    Axios sends query params in an object called params, need a check for it

--- api/services/items.js
+++ api/services/items.js
@@ -9,12 +9,19 @@ const getItems = async query => {
 
   const items = await db.query(searchQuery);
 
-  if (items.length === 0)
+  if (items.length === 0) {
+    if (Object.entries(query).length === 0)
+      throw error(404, "Oho, database is empty.\nBe the first to add an item!");
     throw error(404, `We couldn\'t find items with these criteria!`);
+  }
 
   // images array coming back from mysql as String
   items.forEach(item => {
+    const parsedDeliveryTypes = JSON.parse(item.deliveryType);
+    const parsedCategories = JSON.parse(item.category);
     const parsedImages = JSON.parse(item.images);
+    item.category = parsedCategories;
+    item.deliveryType = parsedDeliveryTypes;
     item.images = parsedImages.filter(i => i !== null);
   });
 
@@ -23,12 +30,16 @@ const getItems = async query => {
 
 // return item by itemId
 const getItemById = async itemId => {
-  const item = await db.query(`${getSelectQuery()} WHERE itemId = ?`, itemId);
+  const item = await db.query(`${getSelectQuery()} WHERE i.id = ?`, itemId);
 
   if (item.length === 0) throw error(404, "Item Doesn't Exist!");
 
+  const parsedDeliveryTypes = JSON.parse(item[0].deliveryType);
+  const parsedCategories = JSON.parse(item[0].category);
   const parsedImages = JSON.parse(item[0].images);
   item[0].images = parsedImages.filter(i => i !== null);
+  item[0].category = parsedCategories;
+  item[0].deliveryType = parsedDeliveryTypes;
 
   return item[0];
 };
@@ -38,14 +49,36 @@ const getItemByUser = async (itemId, userId) => {
   const item = await getItemById(itemId);
 
   const userItem = await db.query(
-    "SELECT * from items WHERE itemId = ? and seller = ?",
-    item.itemId,
+    "SELECT * from items WHERE id = ? and seller = ?",
+    item.id,
     userId
   );
 
   if (userItem.length === 0) throw error(403, "Unauthorized user!");
 
   return userItem[0];
+};
+
+// return all items the belong to a userId
+const getItemsByUser = async userId => {
+  const userItems = await db.query(
+    `${getSelectQuery()} WHERE seller = ?`,
+    userId
+  );
+
+  if (userItems.length === 0)
+    throw error(404, "Oho, you don't have any items!");
+
+  userItems.forEach(item => {
+    const parsedDeliveryTypes = JSON.parse(item.deliveryType);
+    const parsedCategories = JSON.parse(item.category);
+    const parsedImages = JSON.parse(item.images);
+    item.category = parsedCategories;
+    item.deliveryType = parsedDeliveryTypes;
+    item.images = parsedImages.filter(i => i !== null);
+  });
+
+  return userItems;
 };
 
 // create a new item
@@ -107,7 +140,7 @@ const updateItem = async (item, itemId) => {
     `UPDATE items SET 
     title = ?, description = ?, price = ?, country = ?, city = ?, img1 = ?, 
     img2 =  ?, img3 = ?, img4 = ?, category = ?, deliveryType = ?, seller = ?, 
-    updatedAt = ? WHERE itemId = ?`,
+    updatedAt = ? WHERE id = ?`,
     title,
     description,
     price,
@@ -130,7 +163,7 @@ const deleteItem = async (itemId, userId) => {
   await getItemByUser(itemId, userId);
 
   await db.query(
-    "DELETE FROM items WHERE itemId = ? and seller = ?",
+    "DELETE FROM items WHERE id = ? and seller = ?",
     itemId,
     userId
   );
@@ -139,6 +172,7 @@ const deleteItem = async (itemId, userId) => {
 module.exports = {
   getItems,
   getItemById,
+  getItemsByUser,
   createNewItem,
   updateItem,
   deleteItem,
